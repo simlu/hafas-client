1fad32f3719f9288fdc87891a0bc7f95cd34b696
Jannis R <mail@jannisr.de>
2018-11-12 13:45:29 +0100

    INVG profile

---
+++ p/invg/example.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const createClient = require('../..')
+const cmtaProfile = require('.')
+
+const client = createClient(cmtaProfile, 'hafas-client-example')
+
+// Ingolstadt Hbf to Ehringer Tor, Ulm
+client.journeys('8000183', '0801236', {results: 1})
+// .then(([journey]) => {
+// 	const leg = journey.legs[0]
+// 	return client.trip(leg.id, leg.line.name, {polyline: true})
+// })
+// .then(([journey]) => {
+// 	return client.refreshJourney(journey.refreshToken, {stopovers: true, remarks: true})
+// })
+
+// client.departures('8000183', {duration: 5})
+// client.arrivals('8000183', {duration: 10, stationLines: true})
+
+// client.locations('tillystr 1', {results: 2})
+// client.station('000005534') // Downtown light rail station
+// client.nearby({
+// 	type: 'location',
+// 	latitude: 48.74453,
+// 	longitude: 11.43733
+// }, {distance: 200})
+
+// client.radar({
+// 	north: 48.74453,
+// 	west: 11.42733,
+// 	south: 48.73453,
+// 	east: 11.43733
+// }, {results: 10})
+
+// todo: `reachableFrom` with `Ingolstadt, Tillystraße 1` 48.745769 | 11.432814
+
+.then((data) => {
+	console.log(require('util').inspect(data, {depth: null}))
+})
+.catch(console.error)
---
+++ p/invg/products.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = [
+	{
+		id: 'bus',
+		mode: 'bus',
+		bitmasks: [1],
+		name: 'Bus',
+		short: 'Bus',
+		default: true
+	},
+	{
+		id: 'local-train',
+		mode: 'train',
+		bitmasks: [8],
+		name: 'Nahverkehrszug',
+		short: 'Zug',
+		default: true
+	}
+]
